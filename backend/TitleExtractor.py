ba366913f02037e03e0be63fa6587b20485ff79f
essaund2 <essaund2@ncsu.edu>
2025-06-27 14:29:44 -0400

    Implemented ebay scraper

---
+++ backend/TitleExtractor.py
@@ -0,0 +1,68 @@
+import re
+
+class TitleExtractor:
+    with open("brands.txt", "r", encoding="utf-8") as file:
+        brands = [line.strip() for line in file.readlines()]
+    brands_set = set(brands)
+
+    category_keywords = {
+        "Jacket": ["jacket", "parka", "anorak"],
+        "Shirt": ["shirt", "button-up", "flannel"],
+        "Pants": ["pants", "jeans", "trousers", "slacks"],
+        "Hoodie": ["hoodie", "pullover", "sweatshirt"]
+    }
+
+    sub_category_keywords = {
+    "Windbreaker": ["windbreaker"],
+    "Denim": ["denim", "jean"],
+    "Puffer": ["puffer", "down jacket"]
+    }
+
+    def extract_fields(self, title):
+        return {
+            "brand": self.extract_brand(title),
+            "category": self.match_keywords(title, self.category_keywords),
+            "sub_category": self.match_keywords(title, self.sub_category_keywords),
+            "size": self.extract_size(title)
+        }
+
+    def extract_brand(self, title):
+        for word in title.split():
+            clean_word = word.strip(",.!?").title()
+            if clean_word in self.brands_set:
+                return clean_word
+        return "Unknown"
+    
+    def match_keywords(self, title, keyword_map):
+        title = title.lower()
+        for label, keywords in keyword_map.items():
+            for word in keywords:
+                if word in title:
+                    return label
+        return "Unknown"
+    
+    def extract_size(self, title):
+        title = title.lower()
+
+        full_words = {
+            "extra small": "XS",
+            "small": "S",
+            "medium": "M",
+            "large": "L",
+            "extra large": "XL",
+            "xxl": "XXL"
+        }
+
+        for word, code in full_words.items():
+            if word in title:
+                return code
+        
+        match = re.search(r"\b(XXL|XL|XS|S|M|L)\b", title.upper())
+        if match:
+            return match.group().upper()
+        
+        match = re.search(r"\b\d{1,2}x\d{1,2}\b|\b\d{1,2}\b", title)
+        if match:
+            return match.group()
+        
+        return "Unknown"
