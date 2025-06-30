dcc4cfe2c86a2429010dea42c669af9d05af0068
esaunders11 <esaunders1104@gmail.com>
2025-06-29 23:23:33 -0400

    Delete backend/test.py

--- backend/test.py
+++
@@ -1,8 +0,0 @@
-from TitleExtractor import TitleExtractor
-
-title_extractor = TitleExtractor()
-
-title = "Ancient Greek Sandals Half off sale"
-brand = title_extractor.extract_brand(title)
-size = title_extractor.extract_size("Size: L")
-print(brand, size)
